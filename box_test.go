dc441929d4aeff0811769f21dd28a363a02c3f03
lemon-mint <ice.lemons.mint@gmail.com>
2021-10-18 16:42:04 +0900

    Add Test

---
+++ box_test.go
@@ -0,0 +1,39 @@
+package vbox
+
+import (
+	"bytes"
+	"testing"
+)
+
+var plaintext = []byte(`
+Hello, world!
+
+This is a super-secret message.
+do not share!
+
+This message is encrypted with the AES256-GCM cipher and XChaCha20-Poly1305.
+So, it is highly encrypted!
+
+The key of the this message are hashed with blake3-512 Alogrithm.
+`)
+
+func TestBlackBox(t *testing.T) {
+	box := NewBlackBox([]byte("test"))
+	sealed := box.Seal(plaintext)
+	opened, ok := box.Open(sealed)
+	if !ok {
+		t.Error("Failed to open sealed message")
+	}
+	if !bytes.Equal(plaintext, opened) {
+		t.Error("Failed to open sealed message")
+	}
+}
+
+func BenchmarkBlackBox(b *testing.B) {
+	box := NewBlackBox([]byte("test"))
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			box.Seal(plaintext)
+		}
+	})
+}
